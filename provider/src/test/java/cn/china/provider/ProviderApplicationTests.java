3fcf32c57001cef8d96ca35f091fa4b190a5123f
GoodLuckss <1933961936@qq.com>
2020-09-11 15:29:19 +0800

    Initial commit

---
+++ provider/src/test/java/cn/china/provider/ProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.china.provider;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
